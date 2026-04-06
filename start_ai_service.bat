a7c7cfa62a294539d5f721cff775bf4925588400
sahilawatramani <sahilawatramani@gmail.com>
2026-04-06 13:27:03 +0530

    Configure Ngrok authtoken inside batch script

--- start_ai_service.bat
+++ start_ai_service.bat
@@ -10,6 +10,9 @@ start "UniConnect AI Service (DO NOT CLOSE)" cmd /k "cd ai-service && title UniC
 echo Waiting 3 seconds for server to boot...
 timeout /t 3 /nobreak >nul
 
+echo Configuring Ngrok Authtoken...
+.\ngrok.exe config add-authtoken 3ByUGccHf8SGsxVlRSqa3mWxtEL_5wBZP8hYFLDPdTvxkinCW
+
 echo Starting Ngrok Tunnel...
 start "Ngrok Tunnel (DO NOT CLOSE)" cmd /k "title Ngrok Tunnel && .\ngrok.exe http --domain=formalistic-political-cortney.ngrok-free.dev 8000"
 
